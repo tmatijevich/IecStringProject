7dc31431a339e2abdb17179236b1bbee0ed14f78
Tyler Matijevich <tyler.matijevich@br-automation.com>
2023-09-18 17:40:23 -0500

    Complete tests for copy

--- Logical/TestSuite/test_copy.c
+++ Logical/TestSuite/test_copy.c
@@ -22,8 +22,68 @@ _TEST copy_basic(void) {
     TEST_DONE;
 }
 
+_TEST copy_null(void) {
+    char a[81];
+    int32_t status;
+
+    strcpy(a, "Test");
+
+    status = IecStringCopy(a, sizeof(a), NULL);
+
+    TEST_ASSERT_EQUAL_STRING("Test", a);
+    TEST_ASSERT_EQUAL_INT(IECSTRING_ERROR_NULL, status);
+
+    TEST_DONE;
+}
+
+_TEST copy_size(void) {
+    char a[81], b[81];
+    int32_t status;
+
+    strcpy(a, "Test");
+
+    status = IecStringCopy(a, 0, b);
+
+    TEST_ASSERT_EQUAL_STRING("Test", a);
+    TEST_ASSERT_EQUAL_INT(IECSTRING_ERROR_SIZE, status);
+
+    TEST_DONE;
+}
+
+_TEST copy_overlap(void) {
+    char a[81];
+    int32_t status;
+
+    strcpy(a, "Test overlap");
+
+    status = IecStringCopy(a, sizeof(a), a + 5);
+
+    TEST_ASSERT_EQUAL_STRING("Test overlap", a);
+    TEST_ASSERT_EQUAL_INT(IECSTRING_ERROR_OVERLAP, status);
+
+    TEST_DONE;
+}
+
+_TEST copy_truncate(void) {
+    char a[11], b[81];
+    int32_t status;
+
+    strcpy(b, "0123456789abc");
+
+    status = IecStringCopy(a, sizeof(a), b);
+
+    TEST_ASSERT_EQUAL_STRING("0123456789", a);
+    TEST_ASSERT_EQUAL_INT(IECSTRING_WARNING_TRUNCATE, status);
+
+    TEST_DONE;
+}
+
 UNITTEST_FIXTURES(fixtures) {
-    new_TestFixture("IecStringCopy basic", copy_basic)
+    new_TestFixture("IecStringCopy basic", copy_basic),
+    new_TestFixture("IecStringCopy null", copy_null),
+    new_TestFixture("IecStringCopy size", copy_size),
+    new_TestFixture("IecStringCopy overlap", copy_overlap),
+    new_TestFixture("IecStringCopy truncate", copy_truncate)
 };
 
 UNITTEST_CALLER_TEST (copy_set, "IecStringCopy test set", fixtures);
