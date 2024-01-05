f3a9874bd30669a1f8342288c2fd188beda0cd6f
Tyler Matijevich <tyler.matijevich@br-automation.com>
2024-01-04 19:13:15 -0600

    Finish performance testing of copy function.  Create global variable in C programs to reduce cyclic function stack.

--- Logical/Source/Performance/ACopy3Brs/Main.c
+++ Logical/Source/Performance/ACopy3Brs/Main.c
@@ -2,10 +2,10 @@
 #include <stdint.h>
 #include <AsBrStr.h>
 
-void _CYCLIC ProgramCyclic(void)
-{
-    char a[81];
-    int i;
+char a[81];
+int i;
+
+void _CYCLIC ProgramCyclic(void) {
     for (i = 0; i < 100; i++)
         brsstrcpy((uint32_t)a, (uint32_t)"This is a test of string copy performance across multiple libraries.");
 }
