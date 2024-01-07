1a2cb624128ff1f6cac789a9e7cdb99a061bd422
Tyler Matijevich <tylermatijevich@gmail.com>
2024-01-06 19:23:11 -0600

    Update copy tests, add another standard test and add another overlap test

--- Logical/Source/UnitTest/TestSuite/test_copy.c
+++ Logical/Source/UnitTest/TestSuite/test_copy.c
@@ -1,16 +1,22 @@
-
 #include <UnitTest.h>
 #include <IecString.h>
 #include <string.h>
 #include <stdint.h>
 
-_TEST copy_basic(void) {
+_TEST test_copy_standard(void) {
     char a[81], b[81];
     int32_t status;
 
     strcpy(b, "Hello World!");
 
+    /* Copy from local variable */
     status = IecStringCopy(a, sizeof(a), b);
+
+    TEST_ASSERT_EQUAL_STRING("Hello World!", a);
+    TEST_ASSERT_EQUAL_INT(0, status);
+
+    /* Copy from literal */
+    status = IecStringCopy(a, sizeof(a), "Hello World!");
 
     TEST_ASSERT_EQUAL_STRING("Hello World!", a);
     TEST_ASSERT_EQUAL_INT(0, status);
@@ -18,21 +24,21 @@ _TEST copy_basic(void) {
     TEST_DONE;
 }
 
-_TEST copy_null_source(void) {
+_TEST test_copy_null_source(void) {
     char a[81];
     int32_t status;
 
-    strcpy(a, "Test");
+    strcpy(a, "Hello World!");
 
     status = IecStringCopy(a, sizeof(a), NULL);
 
-    TEST_ASSERT_EQUAL_STRING("Test", a);
+    TEST_ASSERT_EQUAL_STRING("Hello World!", a);
     TEST_ASSERT_EQUAL_INT(IECSTRING_ERROR_NULL, status);
 
     TEST_DONE;
 }
 
-_TEST copy_null_destination(void) {
+_TEST test_copy_null_destination(void) {
     char b[81];
     int32_t status;
 
@@ -45,35 +51,53 @@ _TEST copy_null_destination(void) {
     TEST_DONE;
 }
 
-_TEST copy_size(void) {
+_TEST test_copy_size(void) {
     char a[81], b[81];
     int32_t status;
 
-    strcpy(a, "Test");
+    strcpy(a, "Hello World!");
 
     status = IecStringCopy(a, 0, b);
 
-    TEST_ASSERT_EQUAL_STRING("Test", a);
+    TEST_ASSERT_EQUAL_STRING("Hello World!", a);
     TEST_ASSERT_EQUAL_INT(IECSTRING_ERROR_SIZE, status);
 
     TEST_DONE;
 }
 
-_TEST copy_overlap(void) {
+_TEST test_copy_overlap_destination(void) {
     char a[81];
+    char* b;
     int32_t status;
 
-    strcpy(a, "Test overlap");
+    strcpy(a, "Hello World!");
+    b = a + 6;
 
-    status = IecStringCopy(a, sizeof(a), a + 5);
+    status = IecStringCopy(a, sizeof(a), b);
 
-    TEST_ASSERT_EQUAL_STRING("Test overlap", a);
+    TEST_ASSERT_EQUAL_STRING("Hello World!", a);
     TEST_ASSERT_EQUAL_INT(IECSTRING_ERROR_OVERLAP, status);
 
     TEST_DONE;
 }
 
-_TEST copy_truncate(void) {
+_TEST test_copy_overlap_source(void) {
+    char* a;
+    char b[81];
+    int32_t status;
+
+    strcpy(b, "Hello World!");
+    a = b + 6;
+
+    status = IecStringCopy(a, 6, b);
+
+    TEST_ASSERT_EQUAL_STRING("World!", a);
+    TEST_ASSERT_EQUAL_INT(IECSTRING_ERROR_OVERLAP, status);
+
+    TEST_DONE;
+}
+
+_TEST test_copy_truncate(void) {
     char a[11], b[81];
     int32_t status;
 
@@ -88,12 +112,15 @@ _TEST copy_truncate(void) {
 }
 
 UNITTEST_FIXTURES(fixtures) {
-    new_TestFixture("IecStringCopy basic", copy_basic),
-    new_TestFixture("IecStringCopy null source", copy_null_source),
-    new_TestFixture("IecStringCopy null destination", copy_null_destination),
-    new_TestFixture("IecStringCopy size", copy_size),
-    new_TestFixture("IecStringCopy overlap", copy_overlap),
-    new_TestFixture("IecStringCopy truncate", copy_truncate)
+    new_TestFixture("IecStringCopy standard", test_copy_standard),
+    new_TestFixture("IecStringCopy null source", test_copy_null_source),
+    new_TestFixture("IecStringCopy null destination", 
+                    test_copy_null_destination),
+    new_TestFixture("IecStringCopy size", test_copy_size),
+    new_TestFixture("IecStringCopy overlap destination", 
+                    test_copy_overlap_destination),
+    new_TestFixture("IecStringCopy overlap source", test_copy_overlap_source),
+    new_TestFixture("IecStringCopy truncate", test_copy_truncate)
 };
 
 UNITTEST_CALLER_TEST (copy_set, "IecStringCopy test set", fixtures);
