9fbba3fd80860d1ce74f82073f2e81456dbf5f42
Tyler Matijevich <tyler.matijevich@br-automation.com>
2024-01-09 17:39:07 -0600

    Update and add copy tests

--- Logical/Source/UnitTest/TestSuite/test_copy.c
+++ Logical/Source/UnitTest/TestSuite/test_copy.c
@@ -1,81 +1,142 @@
 #include <UnitTest.h>
 #include <IecString.h>
-#include <string.h>
 #include <stdint.h>
+#include <string.h>
 
-_TEST test_copy_standard(void) {
-    char a[81], b[81];
+#define SAMPLE_SIZE 81
+#define SAMPLE_STRING "Hello World!"
+#define LIBRARY_ERROR_NONE 0
+#define LARGE_SIZE 1001
+
+_TEST test_copy_variable(void) {
+    char a[SAMPLE_SIZE], b[SAMPLE_SIZE];
     int32_t status;
 
-    strcpy(b, "Hello World!");
+    strcpy(b, SAMPLE_STRING);
 
-    /* Copy from local variable */
     status = IecStringCopy(a, sizeof(a), b);
 
-    TEST_ASSERT_EQUAL_STRING("Hello World!", a);
-    TEST_ASSERT_EQUAL_INT(0, status);
+    TEST_ASSERT_EQUAL_STRING(SAMPLE_STRING, a);
+    TEST_ASSERT_EQUAL_INT(LIBRARY_ERROR_NONE, status);
 
-    /* Copy from literal */
-    status = IecStringCopy(a, sizeof(a), "Hello World!");
+    TEST_DONE;
+}
 
-    TEST_ASSERT_EQUAL_STRING("Hello World!", a);
-    TEST_ASSERT_EQUAL_INT(0, status);
+_TEST test_copy_literal(void) {
+    char a[SAMPLE_SIZE];
+    int32_t status;
+
+    status = IecStringCopy(a, sizeof(a), SAMPLE_STRING);
+
+    TEST_ASSERT_EQUAL_STRING(SAMPLE_STRING, a);
+    TEST_ASSERT_EQUAL_INT(LIBRARY_ERROR_NONE, status);
+
+    TEST_DONE;
+}
+
+_TEST test_copy_empty_source(void) {
+    char a[SAMPLE_SIZE], b[SAMPLE_SIZE];
+    int32_t status;
+
+    /* Initialize a to sample */
+    strcpy(a, SAMPLE_STRING);
+    strcpy(b, "");
+
+    status = IecStringCopy(a, sizeof(a), b);
+
+    TEST_ASSERT_EQUAL_STRING("", a);
+    TEST_ASSERT_EQUAL_INT(LIBRARY_ERROR_NONE, status);
+
+    TEST_DONE;
+}
+
+_TEST test_copy_size_1(void) {
+    char a[SAMPLE_SIZE], b[SAMPLE_SIZE];
+    int32_t status;
+
+    /* Initialize a to sample */
+    strcpy(a, SAMPLE_STRING);
+    strcpy(b, SAMPLE_STRING);
+
+    status = IecStringCopy(a, 1, b);
+
+    TEST_ASSERT_EQUAL_STRING("", a);
+    TEST_ASSERT_EQUAL_INT(IECSTRING_WARNING_TRUNCATE, status);
+
+    TEST_DONE;
+}
+
+_TEST test_copy_size_large(void) {
+    char a[LARGE_SIZE], b[LARGE_SIZE];
+    int32_t status;
+
+    /* Initialize b */
+    memset(b, 'z', LARGE_SIZE - 1);
+    b[LARGE_SIZE - 1] = '\0';
+
+    status = IecStringCopy(a, sizeof(a), b);
+
+    TEST_ASSERT_EQUAL_STRING(b, a);
+    TEST_ASSERT_EQUAL_INT(LIBRARY_ERROR_NONE, status);
 
     TEST_DONE;
 }
 
 _TEST test_copy_null_source(void) {
-    char a[81];
+    char a[SAMPLE_SIZE];
     int32_t status;
 
-    strcpy(a, "Hello World!");
+    strcpy(a, SAMPLE_STRING);
 
     status = IecStringCopy(a, sizeof(a), NULL);
 
-    TEST_ASSERT_EQUAL_STRING("Hello World!", a);
+    TEST_ASSERT_EQUAL_STRING(SAMPLE_STRING, a);
     TEST_ASSERT_EQUAL_INT(IECSTRING_ERROR_NULL, status);
 
     TEST_DONE;
 }
 
 _TEST test_copy_null_destination(void) {
-    char b[81];
+    char b[SAMPLE_SIZE];
     int32_t status;
 
-    strcpy(b, "Test");
+    strcpy(b, SAMPLE_STRING);
 
-    status = IecStringCopy(NULL, 10, b);
+    status = IecStringCopy(NULL, SAMPLE_SIZE, b);
 
     TEST_ASSERT_EQUAL_INT(IECSTRING_ERROR_NULL, status);
 
     TEST_DONE;
 }
 
-_TEST test_copy_size(void) {
-    char a[81], b[81];
+_TEST test_copy_size_zero(void) {
+    char a[SAMPLE_SIZE], b[SAMPLE_SIZE];
     int32_t status;
 
-    strcpy(a, "Hello World!");
+    /* Initialize a to sample */
+    strcpy(a, SAMPLE_STRING);
 
     status = IecStringCopy(a, 0, b);
 
-    TEST_ASSERT_EQUAL_STRING("Hello World!", a);
+    TEST_ASSERT_EQUAL_STRING(SAMPLE_STRING, a);
     TEST_ASSERT_EQUAL_INT(IECSTRING_ERROR_SIZE, status);
 
     TEST_DONE;
 }
 
 _TEST test_copy_overlap_destination(void) {
-    char a[81];
+    char a[SAMPLE_SIZE];
     char* b;
     int32_t status;
 
-    strcpy(a, "Hello World!");
-    b = a + 6;
+    /* Initialize a to sample */
+    strcpy(a, SAMPLE_STRING);
+    /* Assign source to overlap destination */
+    b = a + sizeof(SAMPLE_STRING) / 2;
 
     status = IecStringCopy(a, sizeof(a), b);
 
-    TEST_ASSERT_EQUAL_STRING("Hello World!", a);
+    TEST_ASSERT_EQUAL_STRING(SAMPLE_STRING, a);
     TEST_ASSERT_EQUAL_INT(IECSTRING_ERROR_OVERLAP, status);
 
     TEST_DONE;
@@ -83,22 +144,25 @@ _TEST test_copy_overlap_destination(void) {
 
 _TEST test_copy_overlap_source(void) {
     char* a;
-    char b[81];
+    char b[SAMPLE_SIZE];
+    char c[SAMPLE_SIZE];
     int32_t status;
 
-    strcpy(b, "Hello World!");
-    a = b + 6;
+    strcpy(b, SAMPLE_STRING);
+    /* Assign destination to overlap source */
+    a = b + sizeof(SAMPLE_STRING) / 2;
+    strcpy(c, a);
 
-    status = IecStringCopy(a, 6, b);
+    status = IecStringCopy(a, SAMPLE_SIZE / 2, b);
 
-    TEST_ASSERT_EQUAL_STRING("World!", a);
+    TEST_ASSERT_EQUAL_STRING(c, a);
     TEST_ASSERT_EQUAL_INT(IECSTRING_ERROR_OVERLAP, status);
 
     TEST_DONE;
 }
 
 _TEST test_copy_truncate(void) {
-    char a[11], b[81];
+    char a[11], b[SAMPLE_SIZE];
     int32_t status;
 
     strcpy(b, "0123456789abc");
@@ -112,11 +176,15 @@ _TEST test_copy_truncate(void) {
 }
 
 UNITTEST_FIXTURES(fixtures) {
-    new_TestFixture("IecStringCopy standard", test_copy_standard),
+    new_TestFixture("IecStringCopy from variable", test_copy_variable),
+    new_TestFixture("IecStringCopy from literal", test_copy_literal),
+    new_TestFixture("IecStringCopy empty source", test_copy_empty_source),
+    new_TestFixture("IecStringCopy source size 1", test_copy_size_1),
+    new_TestFixture("IecStringCopy source size large", test_copy_size_large),
     new_TestFixture("IecStringCopy null source", test_copy_null_source),
     new_TestFixture("IecStringCopy null destination", 
                     test_copy_null_destination),
-    new_TestFixture("IecStringCopy size", test_copy_size),
+    new_TestFixture("IecStringCopy zero size", test_copy_size_zero),
     new_TestFixture("IecStringCopy overlap destination", 
                     test_copy_overlap_destination),
     new_TestFixture("IecStringCopy overlap source", test_copy_overlap_source),
