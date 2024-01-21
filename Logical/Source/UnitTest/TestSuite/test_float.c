a0cc1af4be5b3f258358d57c84f2e8d87ee286df
Tyler Matijevich <tylermatijevich@gmail.com>
2024-01-21 14:39:15 -0600

    Re-write float exponent tests

--- Logical/Source/UnitTest/TestSuite/test_float.c
+++ Logical/Source/UnitTest/TestSuite/test_float.c
@@ -36,147 +36,161 @@ _TEST test_float_literal(void) {
     TEST_DONE;
 }
 
-_TEST test_float_10_5(void) {
-    char a[81];
+_TEST test_float_exponent_p5(void) {
+    char a[SAMPLE_SIZE];
     int32_t status;
 
-    status = IecStringFloat(a, sizeof(a), 123456.f, 0, 6, 0);
+    status = IecStringFloat(a, sizeof(a), 123456.7f, 
+                            DEFAULT_WIDTH, FULL_PRECISION, IECSTRING_FLAG_NONE);
 
-    TEST_ASSERT_EQUAL_STRING("123456", a);
-    TEST_ASSERT_EQUAL_INT(0, status);
+    TEST_ASSERT_EQUAL_STRING("123456.700000", a);
+    TEST_ASSERT_EQUAL_INT(IECSTRING_ERROR_NONE, status);
 
     TEST_DONE;
 }
 
-_TEST test_float_10_4(void) {
-    char a[81];
+_TEST test_float_exponent_p4(void) {
+    char a[SAMPLE_SIZE];
     int32_t status;
 
-    status = IecStringFloat(a, sizeof(a), 12345.6f, 0, 6, 0);
+    status = IecStringFloat(a, sizeof(a), 12345.67f, 
+                            DEFAULT_WIDTH, FULL_PRECISION, IECSTRING_FLAG_NONE);
 
-    TEST_ASSERT_EQUAL_STRING("12345.6", a);
-    TEST_ASSERT_EQUAL_INT(0, status);
+    TEST_ASSERT_EQUAL_STRING("12345.670000", a);
+    TEST_ASSERT_EQUAL_INT(IECSTRING_ERROR_NONE, status);
 
     TEST_DONE;
 }
 
-_TEST test_float_10_3(void) {
-    char a[81];
+_TEST test_float_exponent_p3(void) {
+    char a[SAMPLE_SIZE];
     int32_t status;
 
-    status = IecStringFloat(a, sizeof(a), 1234.56f, 0, 6, 0);
+    status = IecStringFloat(a, sizeof(a), 1234.567f, 
+                            DEFAULT_WIDTH, FULL_PRECISION, IECSTRING_FLAG_NONE);
 
-    TEST_ASSERT_EQUAL_STRING("1234.56", a);
-    TEST_ASSERT_EQUAL_INT(0, status);
+    TEST_ASSERT_EQUAL_STRING("1234.567000", a);
+    TEST_ASSERT_EQUAL_INT(IECSTRING_ERROR_NONE, status);
 
     TEST_DONE;
 }
 
-_TEST test_float_10_2(void) {
-    char a[81];
+_TEST test_float_exponent_p2(void) {
+    char a[SAMPLE_SIZE];
     int32_t status;
 
-    status = IecStringFloat(a, sizeof(a), 123.456f, 0, 6, 0);
+    status = IecStringFloat(a, sizeof(a), 123.4567f, 
+                            DEFAULT_WIDTH, FULL_PRECISION, IECSTRING_FLAG_NONE);
 
-    TEST_ASSERT_EQUAL_STRING("123.456", a);
-    TEST_ASSERT_EQUAL_INT(0, status);
+    TEST_ASSERT_EQUAL_STRING("123.456700", a);
+    TEST_ASSERT_EQUAL_INT(IECSTRING_ERROR_NONE, status);
 
     TEST_DONE;
 }
 
-_TEST test_float_10_1(void) {
-    char a[81];
+_TEST test_float_exponent_p1(void) {
+    char a[SAMPLE_SIZE];
     int32_t status;
 
-    status = IecStringFloat(a, sizeof(a), 12.3456f, 0, 6, 0);
+    status = IecStringFloat(a, sizeof(a), 12.34567f, 
+                            DEFAULT_WIDTH, FULL_PRECISION, IECSTRING_FLAG_NONE);
 
-    TEST_ASSERT_EQUAL_STRING("12.3456", a);
-    TEST_ASSERT_EQUAL_INT(0, status);
+    TEST_ASSERT_EQUAL_STRING("12.345670", a);
+    TEST_ASSERT_EQUAL_INT(IECSTRING_ERROR_NONE, status);
 
     TEST_DONE;
 }
 
-_TEST test_float_10_0(void) {
-    char a[81];
+_TEST test_float_exponent_0(void) {
+    char a[SAMPLE_SIZE];
     int32_t status;
 
-    status = IecStringFloat(a, sizeof(a), 1.23456f, 0, 6, 0);
+    status = IecStringFloat(a, sizeof(a), 1.234567f, 
+                            DEFAULT_WIDTH, FULL_PRECISION, IECSTRING_FLAG_NONE);
 
-    TEST_ASSERT_EQUAL_STRING("1.23456", a);
-    TEST_ASSERT_EQUAL_INT(0, status);
+    TEST_ASSERT_EQUAL_STRING("1.234567", a);
+    TEST_ASSERT_EQUAL_INT(IECSTRING_ERROR_NONE, status);
 
     TEST_DONE;
 }
 
-_TEST test_float_10_n1(void) {
-    char a[81];
+_TEST test_float_exponent_n1(void) {
+    char a[SAMPLE_SIZE];
     int32_t status;
 
-    status = IecStringFloat(a, sizeof(a), 0.123456f, 0, 6, 0);
+    status = IecStringFloat(a, sizeof(a), 0.1234567f, 
+                            DEFAULT_WIDTH, FULL_PRECISION, IECSTRING_FLAG_NONE);
 
-    TEST_ASSERT_EQUAL_STRING("0.123456", a);
-    TEST_ASSERT_EQUAL_INT(0, status);
+    /* Rounding */
+    TEST_ASSERT_EQUAL_STRING("0.123457", a);
+    TEST_ASSERT_EQUAL_INT(IECSTRING_ERROR_NONE, status);
 
     TEST_DONE;
 }
 
-_TEST test_float_10_n2(void) {
-    char a[81];
+_TEST test_float_exponent_n2(void) {
+    char a[SAMPLE_SIZE];
     int32_t status;
 
-    status = IecStringFloat(a, sizeof(a), 0.012345f, 0, 6, 0);
+    status = IecStringFloat(a, sizeof(a), 0.01234567f, 
+                            DEFAULT_WIDTH, FULL_PRECISION, IECSTRING_FLAG_NONE);
 
-    TEST_ASSERT_EQUAL_STRING("0.012345", a);
-    TEST_ASSERT_EQUAL_INT(0, status);
+    /* Rounding */
+    TEST_ASSERT_EQUAL_STRING("0.012346", a);
+    TEST_ASSERT_EQUAL_INT(IECSTRING_ERROR_NONE, status);
 
     TEST_DONE;
 }
 
-_TEST test_float_10_n3(void) {
-    char a[81];
+_TEST test_float_exponent_n3(void) {
+    char a[SAMPLE_SIZE];
     int32_t status;
 
-    status = IecStringFloat(a, sizeof(a), 0.001234f, 0, 6, 0);
+    status = IecStringFloat(a, sizeof(a), 0.001234567f, 
+                            DEFAULT_WIDTH, FULL_PRECISION, IECSTRING_FLAG_NONE);
 
-    TEST_ASSERT_EQUAL_STRING("0.001234", a);
-    TEST_ASSERT_EQUAL_INT(0, status);
+    /* Rounding */
+    TEST_ASSERT_EQUAL_STRING("0.001235", a);
+    TEST_ASSERT_EQUAL_INT(IECSTRING_ERROR_NONE, status);
 
     TEST_DONE;
 }
 
-_TEST test_float_10_n4(void) {
-    char a[81];
+_TEST test_float_exponent_n4(void) {
+    char a[SAMPLE_SIZE];
     int32_t status;
 
-    status = IecStringFloat(a, sizeof(a), 0.000123f, 0, 6, 0);
+    status = IecStringFloat(a, sizeof(a), 0.0001234567f, 
+                            DEFAULT_WIDTH, FULL_PRECISION, IECSTRING_FLAG_NONE);
 
     TEST_ASSERT_EQUAL_STRING("0.000123", a);
-    TEST_ASSERT_EQUAL_INT(0, status);
+    TEST_ASSERT_EQUAL_INT(IECSTRING_ERROR_NONE, status);
 
     TEST_DONE;
 }
 
-_TEST test_float_10_n5(void) {
-    char a[81];
+_TEST test_float_exponent_n5(void) {
+    char a[SAMPLE_SIZE];
     int32_t status;
 
-    status = IecStringFloat(a, sizeof(a), 0.000012f, 0, 6, 0);
+    status = IecStringFloat(a, sizeof(a), 0.00001234567f, 
+                            DEFAULT_WIDTH, FULL_PRECISION, IECSTRING_FLAG_NONE);
 
     TEST_ASSERT_EQUAL_STRING("0.000012", a);
-    TEST_ASSERT_EQUAL_INT(0, status);
+    TEST_ASSERT_EQUAL_INT(IECSTRING_ERROR_NONE, status);
 
     TEST_DONE;
 }
 
-_TEST test_float_10_n6(void) {
-    char a[81];
+_TEST test_float_exponent_n6(void) {
+    char a[SAMPLE_SIZE];
     int32_t status;
 
-    status = IecStringFloat(a, sizeof(a), 0.000001f, 0, 6, 0);
+    status = IecStringFloat(a, sizeof(a), 0.000001234567f, 
+                            DEFAULT_WIDTH, FULL_PRECISION, IECSTRING_FLAG_NONE);
 
-    /* This is stored as 9.999999e-7 */
-    TEST_ASSERT_EQUAL_STRING("1.00000e-6", a);
-    TEST_ASSERT_EQUAL_INT(0, status);
+    TEST_ASSERT_EQUAL_STRING("0.000001", a);
+    TEST_ASSERT_EQUAL_INT(IECSTRING_ERROR_NONE, status);
 
     TEST_DONE;
 }
@@ -268,18 +282,18 @@ _TEST test_float_10_n6_rollover(void) {
 UNITTEST_FIXTURES(fixtures) {
     new_TestFixture("IecStringFloat from variable", test_float_variable),
     new_TestFixture("IecStringFloat from literal", test_float_literal),
-    new_TestFixture("IecStringFloat 10^5", test_float_10_5),
-    new_TestFixture("IecStringFloat 10^4", test_float_10_4),
-    new_TestFixture("IecStringFloat 10^3", test_float_10_3),
-    new_TestFixture("IecStringFloat 10^2", test_float_10_2),
-    new_TestFixture("IecStringFloat 10^1", test_float_10_1),
-    new_TestFixture("IecStringFloat 10^0", test_float_10_0),
-    new_TestFixture("IecStringFloat 10^-1", test_float_10_n1),
-    new_TestFixture("IecStringFloat 10^-2", test_float_10_n2),
-    new_TestFixture("IecStringFloat 10^-3", test_float_10_n3),
-    new_TestFixture("IecStringFloat 10^-4", test_float_10_n4),
-    new_TestFixture("IecStringFloat 10^-5", test_float_10_n5),
-    new_TestFixture("IecStringFloat 10^-6", test_float_10_n6),
+    new_TestFixture("IecStringFloat exponent +5", test_float_exponent_p5),
+    new_TestFixture("IecStringFloat exponent +4", test_float_exponent_p4),
+    new_TestFixture("IecStringFloat exponent +3", test_float_exponent_p3),
+    new_TestFixture("IecStringFloat exponent +2", test_float_exponent_p2),
+    new_TestFixture("IecStringFloat exponent +1", test_float_exponent_p1),
+    new_TestFixture("IecStringFloat exponent  0", test_float_exponent_0),
+    new_TestFixture("IecStringFloat exponent -1", test_float_exponent_n1),
+    new_TestFixture("IecStringFloat exponent -2", test_float_exponent_n2),
+    new_TestFixture("IecStringFloat exponent -3", test_float_exponent_n3),
+    new_TestFixture("IecStringFloat exponent -4", test_float_exponent_n4),
+    new_TestFixture("IecStringFloat exponent -5", test_float_exponent_n5),
+    new_TestFixture("IecStringFloat exponent -6", test_float_exponent_n6),
     new_TestFixture("IecStringFloat 10^5 rounding", test_float_10_5_round),
     new_TestFixture("IecStringFloat 10^2 rounding", test_float_10_2_round),
     new_TestFixture("IecStringFloat 10^-3 rounding", test_float_10_n3_round),
