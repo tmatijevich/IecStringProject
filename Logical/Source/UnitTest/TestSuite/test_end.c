f6ed4e2a76966408ac95909c20288b87ffde0224
Tyler Matijevich <tylermatijevich@gmail.com>
2024-03-24 11:18:11 -0500

    Complete tests for ends with function

--- Logical/Source/UnitTest/TestSuite/test_end.c
+++ Logical/Source/UnitTest/TestSuite/test_end.c
@@ -28,9 +28,79 @@ _TEST test_end_variable(void)
     TEST_DONE;
 }
 
+_TEST test_end_literal(void)
+{
+    uint8_t result = IecStringEndsWith(STRING_SAMPLE_GREATER, STRING_MATCH);
+
+    TEST_ASSERT_EQUAL_INT(true, result);
+
+    TEST_DONE;
+}
+
+_TEST test_end_equal_match(void)
+{
+    uint8_t result = IecStringEndsWith(STRING_SAMPLE_EQUAL, STRING_MATCH);
+
+    TEST_ASSERT_EQUAL_INT(true, result);
+
+    TEST_DONE;
+}
+
+_TEST test_end_greater_no_match(void)
+{
+    uint8_t result = IecStringEndsWith(STRING_SAMPLE_GREATER, STRING_NO_MATCH);
+
+    TEST_ASSERT_EQUAL_INT(false, result);
+
+    TEST_DONE;
+}
+
+_TEST test_end_equal_no_match(void)
+{
+    uint8_t result = IecStringEndsWith(STRING_SAMPLE_EQUAL, STRING_NO_MATCH);
+
+    TEST_ASSERT_EQUAL_INT(false, result);
+
+    TEST_DONE;
+}
+
+_TEST test_end_lesser(void)
+{
+    uint8_t result = IecStringEndsWith(STRING_SAMPLE_LESSER, STRING_MATCH);
+
+    TEST_ASSERT_EQUAL_INT(false, result);
+
+    TEST_DONE;
+}
+
+_TEST test_end_null_1(void)
+{
+    uint8_t result = IecStringEndsWith(NULL, STRING_MATCH);
+
+    TEST_ASSERT_EQUAL_INT(false, result);
+
+    TEST_DONE;
+}
+
+_TEST test_end_null_2(void)
+{
+    uint8_t result = IecStringEndsWith(STRING_SAMPLE_GREATER, NULL);
+
+    TEST_ASSERT_EQUAL_INT(false, result);
+
+    TEST_DONE;
+}
+
 UNITTEST_FIXTURES(fixtures)
 {
-    new_TestFixture("IecStringEndsWith from variables", test_end_variable)
+    new_TestFixture("IecStringEndsWith from variables", test_end_variable),
+    new_TestFixture("IecStringEndsWith from literals", test_end_literal),
+    new_TestFixture("IecStringEndsWith equal size match", test_end_equal_match),
+    new_TestFixture("IecStringEndsWith greater size no match", test_end_greater_no_match),
+    new_TestFixture("IecStringEndsWith equal size no match", test_end_equal_no_match),
+    new_TestFixture("IecStringEndsWith lesser size", test_end_lesser),
+    new_TestFixture("IecStringEndsWith null string 1", test_end_null_1),
+    new_TestFixture("IecStringEndsWith null string 2", test_end_null_2)
 };
 
 UNITTEST_CALLER_TEST (end_set, "IecStringEndsWith test set", fixtures);
