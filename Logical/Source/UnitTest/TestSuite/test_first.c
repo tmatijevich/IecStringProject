6d31d8bbd2fd4edd61025872dcb311b4404ecf47
Tyler Matijevich <tylermatijevich@gmail.com>
2024-04-10 23:59:34 -0500

    Improve and finish tests for first function

--- Logical/Source/UnitTest/TestSuite/test_first.c
+++ Logical/Source/UnitTest/TestSuite/test_first.c
@@ -3,76 +3,146 @@
 #include <stdint.h>
 #include <string.h>
 
-#define SIZE_STRING 81
-#define SAMPLE_STRING "abcde12345"
-#define SAMPLE_MULTIPLE "abc123abc123"
-
 _TEST test_first_variable(void)
 {
     char find = '3';
-    char a[SIZE_STRING] = SAMPLE_STRING;
+    char a[] = "abcde12345";
 
     /* Call the function with variable arguments */
-    uint32_t address = IecStringFirst(a, (uint8_t)find);
+    uint32_t address = IecStringFirst(a, find);
 
     /* Test the input string has not be modified */
-    TEST_ASSERT_EQUAL_STRING(a, SAMPLE_STRING);
+    TEST_ASSERT_EQUAL_STRING("abcde12345", a);
     /* Test the result */
-    TEST_ASSERT_EQUAL_INT(&a + 8, address);
+    TEST_ASSERT_EQUAL_INT((unsigned)a + 7, address);
 
     TEST_DONE;
 }
 
 _TEST test_first_literal(void)
 {
-    /* Call the function with literal arguments */
-    uint32_t address = IecStringFirst(SAMPLE_STRING, (uint8_t)'3');
+    char a[] = "abcde12345";
 
+    /* Call the function with literal character */
+    uint32_t address = IecStringFirst(a, '3');
+
+    /* Test the input string has not be modified */
+    TEST_ASSERT_EQUAL_STRING("abcde12345", a);
     /* Test the result */
-    TEST_ASSERT_EQUAL_INT(&SAMPLE_STRING + 8, address);
+    TEST_ASSERT_EQUAL_INT((unsigned)a + 7, address);
 
     TEST_DONE;
 }
 
 _TEST test_first_first(void)
 {
-    uint32_t address = IecStringFirst(SAMPLE_STRING, (uint8_t)'a');
+    char a[] = "abcde12345";
 
-    TEST_ASSERT_EQUAL_INT(&SAMPLE_STRING, address);
+    uint32_t address = IecStringFirst(a, 'a');
+
+    /* Test the input string has not be modified */
+    TEST_ASSERT_EQUAL_STRING("abcde12345", a);
+    /* Test the result */
+    TEST_ASSERT_EQUAL_INT((unsigned)a , address);
 
     TEST_DONE;
 }
 
 _TEST test_first_last(void)
 {
-    uint32_t address = IecStringFirst(SAMPLE_STRING, (uint8_t)'5');
+    char a[] = "abcde12345";
 
-    TEST_ASSERT_EQUAL_INT(&SAMPLE_STRING + strlen(SAMPLE_STRING), address);
+    uint32_t address = IecStringFirst(a, '5');
+
+    /* Test the input string has not be modified */
+    TEST_ASSERT_EQUAL_STRING("abcde12345", a);
+    /* Test the result */
+    TEST_ASSERT_EQUAL_INT((unsigned)a + 9, address);
 
     TEST_DONE;
 }
 
 _TEST test_first_multiple(void)
 {
-    uint32_t address = IecStringFirst(SAMPLE_MULTIPLE, (uint8_t)'b');
+    char b[] = "abc123abc123";
 
-    TEST_ASSERT_EQUAL_INT(&SAMPLE_MULTIPLE + 1, address);
+    uint32_t address = IecStringFirst(b, 'b');
+
+    /* Test the input string has not be modified */
+    TEST_ASSERT_EQUAL_STRING("abc123abc123", b);
+    /* Test the result */
+    TEST_ASSERT_EQUAL_INT((unsigned)b + 1, address);
 
     TEST_DONE;
 }
 
 _TEST test_first_none(void)
 {
-    uint32_t address = IecStringFirst(SAMPLE_MULTIPLE, (uint8_t)'g');
+    char b[] = "abc123abc123";
 
+    uint32_t address = IecStringFirst(b, 'g');
+
+    /* Test the input string has not be modified */
+    TEST_ASSERT_EQUAL_STRING("abc123abc123", b);
+    /* Test the result */
     TEST_ASSERT_EQUAL_INT(0, address);
 
     TEST_DONE;
 }
 
-/* Test special character */
-/* Test non-ascii character */
-/* Test null string */
+_TEST test_first_special(void)
+{
+    char find = 6;
+    char a[] = "abcde12345";
+    memset(a + 3, find, 1);
+
+    /* Call with special character */
+    uint32_t address = IecStringFirst(a, find);
+
+    /* Test the result */
+    TEST_ASSERT_EQUAL_INT((unsigned)a + 3, address);
+
+    TEST_DONE;
+}
+
+_TEST test_first_nonascii(void)
+{
+    char find = 140;
+    char a[] = "abcde12345";
+    memset(a + 3, find, 1);
+
+    /* Call with non-ascii character */
+    uint32_t address = IecStringFirst(a, find);
+
+    /* Test the result */
+    TEST_ASSERT_EQUAL_INT((unsigned)a + 3, address);
+
+    TEST_DONE;
+}
+
+_TEST test_first_empty(void)
+{
+    char a[] = "";
+
+    /* Call with empty string */
+    uint32_t address = IecStringFirst(a, '3');
+
+    /* Test the result */
+    TEST_ASSERT_EQUAL_INT(0, address);
+
+    TEST_DONE;
+}
+
+_TEST test_first_null(void)
+{
+    /* Call with null string */
+    uint32_t address = IecStringFirst(NULL, '3');
+
+    /* Test the result */
+    TEST_ASSERT_EQUAL_INT(0, address);
+
+    TEST_DONE;
+}
 
 UNITTEST_FIXTURES(fixtures) {
     new_TestFixture("IecStringFirst from variable", test_first_variable),
@@ -80,7 +150,11 @@ UNITTEST_FIXTURES(fixtures) {
     new_TestFixture("IecStringFirst first character", test_first_first),
     new_TestFixture("IecStringFirst last character", test_first_last),
     new_TestFixture("IecStringFirst multiple characters", test_first_multiple),
-    new_TestFixture("IecStringFirst zero characters", test_first_none)
+    new_TestFixture("IecStringFirst zero characters", test_first_none),
+    new_TestFixture("IecStringFirst special character", test_first_special),
+    new_TestFixture("IecStringFirst non-ascii character", test_first_nonascii),
+    new_TestFixture("IecStringFirst empty string", test_first_empty),
+    new_TestFixture("IecStringFirst null string", test_first_null)
 };
 
 UNITTEST_CALLER_TEST(first_set, "IecStringFirst test set", fixtures);
