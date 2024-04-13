3485355b62804e3961d92c9cb120cfdda94fce8c
Tyler Matijevich <tylermatijevich@gmail.com>
2024-04-12 22:16:33 -0500

    Update split example and update split and find tests

--- Logical/Source/UnitTest/TestSuite/test_find.c
+++ Logical/Source/UnitTest/TestSuite/test_find.c
@@ -3,9 +3,7 @@
 #include <stdint.h>
 #include <string.h>
 
-#define STRING_SAMPLE "The quick brown fox jumped over the lazy dog"
-#define SAMPLE_SEARCH "Red left his red house wearing \
-a red shirt driving a red car"
+#define STRING_SAMPLE "The quick brown fox jumps over the lazy dog"
 
 _TEST test_find_variable(void)
 {
@@ -23,8 +21,40 @@ _TEST test_find_variable(void)
     TEST_DONE;
 }
 
+_TEST test_find_literal(void)
+{
+    char a[] = STRING_SAMPLE;
+
+    /* Call the function with variable arguments */
+    uint32_t address = IecStringFind(a, "fox");
+
+    /* Test the input string has not be modified */
+    TEST_ASSERT_EQUAL_STRING(STRING_SAMPLE, a);
+    /* Test the result */
+    TEST_ASSERT_EQUAL_INT((unsigned)a + 16, address);
+
+    TEST_DONE;
+}
+
+_TEST test_find_single(void)
+{
+    char a[] = STRING_SAMPLE;
+
+    /* Call the function with variable arguments */
+    uint32_t address = IecStringFind(a, "k");
+
+    /* Test the input string has not be modified */
+    TEST_ASSERT_EQUAL_STRING(STRING_SAMPLE, a);
+    /* Test the result */
+    TEST_ASSERT_EQUAL_INT((unsigned)a + 8, address);
+
+    TEST_DONE;
+}
+
 UNITTEST_FIXTURES(fixtures) {
-    new_TestFixture("IecStringFind from variable", test_find_variable)
+    new_TestFixture("IecStringFind from variable", test_find_variable),
+    new_TestFixture("IecStringFind from literal", test_find_literal),
+    new_TestFixture("IecStringFind single character", test_find_single)
 };
 
 UNITTEST_CALLER_TEST(find_set, "IecStringFind test set", fixtures);
--- Logical/Source/UnitTest/TestSuite/test_split.c
+++ Logical/Source/UnitTest/TestSuite/test_split.c
@@ -3,11 +3,9 @@
 #include <stdint.h>
 #include <string.h>
 
-#define STRING_SAMPLE "The quick brown fox jumps over the lazy dog"
-
 _TEST test_split_variable(void)
 {
-    char a[] = STRING_SAMPLE;
+    char a[] = "The quick brown fox jumps over the lazy dog";
     char delimeters[] = " ";
     char b[5][10] = {{0}};
     int32_t result[5] = {0};
@@ -18,10 +16,12 @@ _TEST test_split_variable(void)
     do
     {
         result[i] = IecStringSplit(b[i], sizeof(b[i]), address, delimeters, 
-                                   &next_address);
+                                   (uint32_t*)&next_address);
         address = next_address;
     }
     while (++i < 5 && next_address);
+
+    TEST_ASSERT_EQUAL_STRING("The quick brown fox jumps over the lazy dog", a);
 
     TEST_ASSERT_EQUAL_STRING("The", b[0]);
     TEST_ASSERT_EQUAL_STRING("quick", b[1]);
@@ -38,8 +38,61 @@ _TEST test_split_variable(void)
     TEST_DONE;
 }
 
+_TEST test_split_multiple(void)
+{
+    char a[] = "... Apple, Banana, Orange, ...";
+    char delimeters[] = " ,.";
+    char b[3][10] = {{0}};
+    int32_t result[3] = {0};
+    char *address = a, *next_address = 0;
+    int i = 0;
+
+    /* Call the function with variable arguments */
+    do
+    {
+        result[i] = IecStringSplit(b[i], sizeof(b[i]), address, delimeters, 
+                                   (uint32_t*)&next_address);
+        address = next_address;
+    }
+    while (++i < 3 && next_address);
+
+    TEST_ASSERT_EQUAL_STRING("... Apple, Banana, Orange, ...", a);
+
+    TEST_ASSERT_EQUAL_STRING("Apple", b[0]);
+    TEST_ASSERT_EQUAL_STRING("Banana", b[1]);
+    TEST_ASSERT_EQUAL_STRING("Orange", b[2]);
+
+    TEST_ASSERT_EQUAL_INT(IECSTRING_ERROR_NONE, result[0]);
+    TEST_ASSERT_EQUAL_INT(IECSTRING_ERROR_NONE, result[1]);
+    TEST_ASSERT_EQUAL_INT(IECSTRING_ERROR_NONE, result[2]);
+
+    TEST_DONE;
+}
+
+_TEST test_split_truncate(void)
+{
+    char a[] = "Three, two, one";
+    char delimeters[] = " ,";
+    char b[4];
+    char *next_address;
+    int32_t result;
+
+    /* Call the function */
+    result = IecStringSplit(b, sizeof(b), a, delimeters, (uint32_t*)&next_address);
+
+    TEST_ASSERT_EQUAL_STRING("Three, two, one", a);
+
+    TEST_ASSERT_EQUAL_STRING("Thr", b);
+
+    TEST_ASSERT_EQUAL_INT(IECSTRING_WARNING_TRUNCATE, result);
+
+    TEST_DONE;
+}
+
 UNITTEST_FIXTURES(fixtures) {
-    new_TestFixture("IecStringSplit from variable", test_split_variable)
+    new_TestFixture("IecStringSplit from variable", test_split_variable),
+    new_TestFixture("IecStringSplit multiple delimeters", test_split_multiple),
+    new_TestFixture("IecStringSplit truncate output", test_split_truncate)
 };
 
 UNITTEST_CALLER_TEST(split_set, "IecStringSplit test set", fixtures);
